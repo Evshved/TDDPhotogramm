60ce1cb073700603018e93833cb5417b097389e7
Shved <snuggleshved@gmail.com>
2016-07-17 22:51:43 +0300

    Add edit of post. Pre-destroys posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,15 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
   private
   def post_params
     params.require(:post).permit(:caption,:image)
